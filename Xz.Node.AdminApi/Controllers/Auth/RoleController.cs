2033500e6a944e181c14a4db62515054a00aedc6
xiangzheng <18896101294@163.com>
2021-08-29 19:05:03 +0800

    feat： 添加角色管理

---
+++ Xz.Node.AdminApi/Controllers/Auth/RoleController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Xz.Node.AdminApi.Controllers.Auth
+{
+    public class RoleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Xz.Node.App/Auth/Role/RoleApp.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xz.Node.App.Auth.Role
+{
+    public class RoleApp
+    {
+
+    }
+}
