b41291a1760d281a2de642f3d5d470bf7e2a931a
xiangzheng <18896101294@163.com>
2021-09-26 09:51:07 +0800

    fix： SignalR增加链接日志

--- Xz.Node.Interactive/Hubs/ServiceHub.cs
+++ Xz.Node.Interactive/Hubs/ServiceHub.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 using System.Threading.Tasks;
 
@@ -9,6 +10,12 @@ namespace Xz.Node.Interactive.Hubs
     /// </summary>
     public class ServiceHub : Hub
     {
+        private readonly ILogger<ServiceHub> _log;
+        public ServiceHub(ILogger<ServiceHub> log)
+        {
+            _log = log;
+        }
+
         public async Task AddToGroup(string groupName)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
@@ -20,6 +27,7 @@ namespace Xz.Node.Interactive.Hubs
         /// <returns></returns>
         public override Task OnConnectedAsync()
         {
+            _log.LogInformation("客户端连接成功");
             Trace.WriteLine("客户端连接成功");
             return base.OnConnectedAsync();
         }
